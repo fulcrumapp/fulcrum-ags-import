42c02b8f2d940932d3cafb673bda1d1385746038
Jason Sanford <jasonsanford@gmail.com>
2014-12-19 11:42:01 -0500

    Add field types

--- field_mappings.js
+++ field_mappings.js
@@ -1,9 +1,11 @@
 var field_mappings = {
-  'esriFieldTypeString': {type: 'TextField'},
-  'esriFieldTypeOID': {type: 'TextField'},
-  'esriFieldTypeDouble': {type: 'TextField', numeric: true},
-  'esriFieldTypeSingle': {type: 'TextField', numeric: true},
-  'esriFieldTypeInteger': {type: 'TextField', numeric: true, format: 'integer'}
+  'esriFieldTypeString'       : {type: 'TextField'},
+  'esriFieldTypeOID'          : {type: 'TextField'},
+  'esriFieldTypeDate'         : {type: 'DateTimeField'},
+  'esriFieldTypeDouble'       : {type: 'TextField', numeric: true},
+  'esriFieldTypeSingle'       : {type: 'TextField', numeric: true},
+  'esriFieldTypeInteger'      : {type: 'TextField', numeric: true, format: 'integer'},
+  'esriFieldTypeSmallInteger' : {type: 'TextField', numeric: true, format: 'integer'},
 };
 
 module.exports = field_mappings;
