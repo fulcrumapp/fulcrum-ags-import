0d228db9b071a42ea075e0d6b6e877063c0d26bb
Jason Sanford <jasonsanford@gmail.com>
2014-12-19 11:27:05 -0500

    Classify FormCreator.

---
+++ form_creator.js
@@ -0,0 +1,88 @@
+var request = require('request');
+var _ = require('lodash');
+
+var fieldMappings = require('./field_mappings');
+var utils = require('./utils');
+
+function normalizeFieldKey(fieldKey) {
+  return fieldKey.toLowerCase();
+}
+
+function agsFieldsToElements (agsFields) {
+  var fulcrumElements = [];
+  var baseElement = {
+    disabled: false,
+    hidden: false,
+    required: false
+  };
+
+  agsFields.forEach(function(agsField) {
+    if (agsField.type in fieldMappings) {
+      var element = _.merge({}, fieldMappings[agsField.type]);
+      element.key = normalizeFieldKey(agsField.name);
+      element.data_name = normalizeFieldKey(agsField.name);
+      element.label = agsField.alias || agsField.name;
+      fulcrumElements.push(_.merge(element, baseElement));
+    } else {
+      console.log('AGS field "' + agsField.name + '"" has unknown type "' + agsField.type + '" ... skipping.');
+    }
+  });
+  return fulcrumElements;
+}
+
+function FormCreator(serviceUrl, fulcrumClient) {
+  this.serviceUrl = serviceUrl;
+  this.fulcrumClient = fulcrumClient;
+
+  this.agsServiceCallback = utils.bind(this.agsServiceCallback, this);
+  this.fulcrumFormCreateCallback = utils.bind(this.fulcrumFormCreateCallback, this);
+
+  this.baseElement = {
+    disabled: false,
+    hidden: false,
+    required: false
+  };
+}
+
+FormCreator.prototype.create = function (callback) {
+  this.callback = callback;
+  this.getAgsService();
+};
+
+FormCreator.prototype.agsServiceCallback = function (error, response, body) {
+  if (error) {
+    this.callback(error);
+  } else {
+    this.createFulcrumForm(body);
+  }
+};
+
+FormCreator.prototype.getAgsService = function () {
+  agsServiceRequestOptions = {
+    uri: this.serviceUrl + '?f=json',
+    json: true
+  };
+
+  request(agsServiceRequestOptions, this.agsServiceCallback);
+};
+
+FormCreator.prototype.fulcrumFormCreateCallback = function (error, form) {
+  if (error) {
+    this.callback(error);
+  } else {
+    console.log('Form created: ' + JSON.stringify(form));
+  }
+};
+
+FormCreator.prototype.createFulcrumForm = function (agsServiceData) {
+  var formPayload = {
+    form: {
+      name: agsServiceData.name,
+      elements: agsFieldsToElements(agsServiceData.fields)
+    }
+  };
+
+  this.fulcrumClient.forms.create(formPayload, this.fulcrumFormCreateCallback);
+};
+
+module.exports = FormCreator;
--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var request = require('request');
 var Fulcrum = require('fulcrum-app');
 var _ = require('lodash');
 
-var fieldMappings = require('./field_mappings');
+var FormCreator = require('./form_creator');
 
 var args = process.argv.slice(2);
 
@@ -16,70 +16,14 @@ if (args.length < 1) {
 var serviceUrl = args[0];
 var apiKey = process.env.FULCRUM_API_KEY;
 
-var fulcrum = new Fulcrum({api_key: apiKey, url: 'http://localhost:3000/api/v2/'});
+var fulcrumClient = new Fulcrum({api_key: apiKey, url: 'http://localhost:3000/api/v2/'});
 
-var baseElement = {
-  disabled: false,
-  hidden: false,
-  required: false
-};
 
-var agsServiceCallback = function (error, response, body) {
+var fromCreatorCallback = function (error, form) {
   if (error) {
-    console.log(error);
-    return;
+    console.log('Error creating form: ' + error);
   }
-  createForm(body);
 };
+var formCreator = new FormCreator(serviceUrl, fulcrumClient);
 
-agsServiceRequestOptions = {
-  uri: serviceUrl + '?f=json',
-  json: true
-};
-
-request(agsServiceRequestOptions, agsServiceCallback);
-
-function createForm (agsServiceData) {
-  var formPayload = {
-    form: {
-      name: agsServiceData.name,
-      elements: agsFieldsToElements(agsServiceData.fields)
-    }
-  };
-
-  var fulcrumFormCreateCallback = function (error, form) {
-    if (error) {
-      console.log(error);
-      return;
-    }
-    console.log('Form created: ' + JSON.stringify(form));
-  };
-
-  fulcrum.forms.create(formPayload, fulcrumFormCreateCallback);
-}
-
-function agsFieldsToElements(agsFields) {
-  var fulcrumElements = [];
-  var baseElement = {
-    disabled: false,
-    hidden: false,
-    required: false
-  };
-
-  agsFields.forEach(function(agsField) {
-    if (agsField.type in fieldMappings) {
-      var element = _.merge({}, fieldMappings[agsField.type]);
-      element.key = normalizeFieldKey(agsField.name);
-      element.data_name = normalizeFieldKey(agsField.name);
-      element.label = agsField.alias || agsField.name;
-      fulcrumElements.push(_.merge(element, baseElement));
-    } else {
-      console.log('AGS field "' + agsField.name + '"" has unknown type "' + agsField.type + '" ... skipping.');
-    }
-  });
-  return fulcrumElements;
-}
-
-function normalizeFieldKey(fieldKey) {
-  return fieldKey.toLowerCase();
-}
+formCreator.create(fromCreatorCallback);
