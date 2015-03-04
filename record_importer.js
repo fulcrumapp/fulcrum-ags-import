8d31cc175e928d0901d19a648a76898afecba5e5
Jason Sanford <jasonsanford@gmail.com>
2015-03-04 11:59:38 -0500

    Use ags-stream to fetch features.

--- record_importer.js
+++ record_importer.js
@@ -1,26 +1,40 @@
 var async = require('async');
 var request = require('request');
 var _ = require('lodash');
+var AgsStream = require('ags-stream')
 
 var utils = require('./utils');
 var esriJsonToGeojson = require('./esrijson_to_geojson');
 var geojsonToPoint = require('./geojson_to_point');
 
 function RecordImporter (form, serviceUrl, options, fulcrumClient) {
+  var me = this;
+
   this.form = form;
-  this.serviceUrl = serviceUrl;
   this.options = options;
   this.fulcrumClient = fulcrumClient;
   this.chunkSize = options.chunkSize;
-  this.parallelLimit = 20;
 
-  this.objectIdsCallback = utils.bind(this.objectIdsCallback, this);
-  this.processChunksCallback = utils.bind(this.processChunksCallback, this);
+  this.agsStream = new AgsStream(serviceUrl, options);
+
+  this.agsStream.on('data', function (data) {
+    data.forEach(function (feature) {
+      me.storeFeature(feature);
+    })
+  });
+
+  this.agsStream.on('error', function (error) {
+    me.callback(error);
+  });
+
+  this.agsStream.on('end', function (data) {
+    me.callback(null);
+  });
 }
 
 RecordImporter.prototype.import = function (callback) {
   this.callback = callback;
-  this.getObjectIds();
+  this.agsStream.read();
 };
 
 RecordImporter.prototype.normalizeAttribute = function (key, value) {
@@ -44,103 +58,15 @@ RecordImporter.prototype.normalizeAttribute = function (key, value) {
   }
 };
 
-RecordImporter.prototype.objectIdsCallback = function (error, response, body) {
-  var chunkSize = this.chunkSize;
-  if (error) {
-    this.callback(error);
-  } else {
-    var objectIds = body.objectIds;
-    console.log('Processing ' + objectIds.length + ' features ...');
-    var chunks = _.chain(objectIds).groupBy(function(element, index){
-      return Math.floor(index / chunkSize);
-    }).toArray().value();
-    this.objectIdChunks = chunks;
-    this.processChunks();
-  }
-};
-
-RecordImporter.prototype.getObjectIds = function () {
-  objectIdsRequestOptions = {
-    uri: this.serviceUrl + '/query',
-    qs: {
-      returnIdsOnly: 'true',
-      f: 'json',
-      where: this.options.where || '1=1'
-    },
-    json: true
-  };
-
-  request(objectIdsRequestOptions, this.objectIdsCallback);
-};
-
-RecordImporter.prototype.processChunks = function () {
+RecordImporter.prototype.storeFeature = function (feature) {
   var me = this;
+  var record = this.agsFeatureToRecord(feature);
 
-  var tasks = this.objectIdChunks.map(function (chunk) {
-    return function(callback) {
-      me.processChunk(chunk, callback);
-    };
-  });
-
-  async.series(tasks, this.processChunksCallback);
-};
-
-RecordImporter.prototype.processChunk = function (chunk, callback) {
-  var me = this;
-  console.log('Processing chunk - size: ' + chunk.length + ', objectids: (' + chunk[0] + ' - ' + chunk[chunk.length - 1] + ').');
-  var tasks = chunk.map(function (objectId) {
-    return function (_callback) {
-      me.fetchAndStoreRecord(objectId, _callback);
-    };
-  });
-
-  async.parallelLimit(tasks, this.parallelLimit, function (error, results) {
+  this.fulcrumClient.records.create(record, function (error, createdRecord) {
     if (error) {
-      callback(error);
-    } else {
-      callback(null, results);
+      me.callback(error);
     }
   });
-};
-
-RecordImporter.prototype.fetchAndStoreRecord = function (objectId, callback) {
-  var me = this;
-
-  var featureRequestOptions = {
-    uri: this.serviceUrl + '/query',
-    qs: {
-      objectIds: objectId,
-      outFields: '*',
-      returnGeometry: 'true',
-      f: 'json',
-      outSR: '4326'
-    },
-    json: true
-  };
-
-  request(featureRequestOptions, function (error, response, body) {
-    if (error) {
-      callback(error);
-    } else {
-      var agsFeature = body.features[0];
-      var record = me.agsFeatureToRecord(agsFeature);
-      me.fulcrumClient.records.create(record, function (error, createdRecord) {
-        if (error) {
-          callback(error);
-        } else {
-          callback(null, 1);
-        }
-      });
-    }
-  });
-};
-
-RecordImporter.prototype.processChunksCallback = function (error, results) {
-  if (error) {
-    this.callback(error);
-  } else {
-    this.callback(null, results);
-  }
 };
 
 RecordImporter.prototype.agsFeatureToRecord = function (agsRecord) {
